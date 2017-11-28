263bcfb64b8cff09dd0ae09d27f6ef205e0b5d84
Stanimir <stambeto2197@gmail.com>
2017-11-28 18:10:27 +0200

    Updated bank login page

--- bankLogin/js/functionality.js
+++ bankLogin/js/functionality.js
@@ -1,21 +1,7 @@
 const loginContent = document.getElementById("loginForm")
 const registerContent = document.getElementById("registerForm")
-const login = document.getElementById("login")
-const register = document.getElementById("register")
-const screenWidth = window.screen.width
-var expandLoginPx = ""
-var shrinkRegisterPx = ""
-var expandRegisterPx = ""
-if(screenWidth <= 480) {
-  expandLoginPx = "50px"
-  shrinkRegisterPx = "-135px"
-  expandRegisterPx = "-60px"
-}
-else {
-  expandLoginPx = "55px"
-  shrinkRegisterPx = "-150px"
-  expandRegisterPx = "-67px"
-}
+const login = document.getElementById("buttonLogin")
+const register = document.getElementById("buttonRegister")
 
 function loginShrinkExpand() {
   if(loginContent.style.visibility === "visible") {
@@ -41,25 +27,21 @@ function registerShrinkExpand() {
 }
 function shrinkLogin() {
   loginContent.style.visibility = "hidden";
-  register.style.marginBottom = "0";
-  login.innerText = "I have already account"
-  register.style.transition = "all 1s";
+  loginContent.style.position = "absolute";
+  login.innerText = "I have already account";
 }
 function expandLogin() {
   loginContent.style.visibility = "visible";
-  register.style.marginBottom = expandLoginPx;
-  login.innerText = "Log in"
-  register.style.transition = "all 1s";
+  loginContent.style.position = "relative";
+  login.innerText = "Log in";
 }
 function shrinkRegister() {
   registerContent.style.visibility = "hidden";
-  register.style.marginTop = shrinkRegisterPx;
-  register.innerText = "Create new account"
-  register.style.transition = "all 1s";
+  registerContent.style.position = "absolute";
+  register.innerText = "Create new account";
 }
 function expandRegister() {
   registerContent.style.visibility = "visible";
-  register.style.marginTop = expandRegisterPx;
-  register.innerText = "Register"
-  register.style.transition = "all 1s";
+  registerContent.style.position = "relative";
+  register.innerText = "Register";
 }
