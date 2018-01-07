971560dcf813ed60f93ecde77150ac18ea398439
Stanimir Iliev <stambeto2197@gmail.com>
2018-01-07 22:53:17 +0200

    Unfinished commit for the bank

--- bankLogin/js/functionality.js
+++ bankLogin/js/functionality.js
@@ -3,18 +3,21 @@ const registerContent = document.getElementById("registerForm")
 const login = document.getElementById("login")
 const register = document.getElementById("register")
 const screenWidth = window.screen.width
-var expandLoginPx = ""
-var shrinkRegisterPx = ""
-var expandRegisterPx = ""
-if(screenWidth <= 480) {
-  expandLoginPx = "50px"
-  shrinkRegisterPx = "-135px"
-  expandRegisterPx = "-60px"
+var loginExpand = ""
+var shrinked = ""
+var registerExpand = ""
+if(screenWidth <= 600) {
+  loginExpand = "50px"
+  shrinked = "-135px"
+  registerExpand = "-60px"
+}
+else if (screenWidth > 600 && screenWidth <= 900) {
+
 }
 else {
-  expandLoginPx = "55px"
-  shrinkRegisterPx = "-150px"
-  expandRegisterPx = "-67px"
+  loginExpand = "120px"
+  shrinked = "180px"
+  registerExpand = "90px"
 }
 
 function loginShrinkExpand() {
@@ -41,25 +44,25 @@ function registerShrinkExpand() {
 }
 function shrinkLogin() {
   loginContent.style.visibility = "hidden";
-  register.style.marginBottom = "0";
+  register.style.bottom = shrinked;
   login.innerText = "I have already account"
   register.style.transition = "all 1s";
 }
 function expandLogin() {
   loginContent.style.visibility = "visible";
-  register.style.marginBottom = expandLoginPx;
+  register.style.bottom = "0";
   login.innerText = "Log in"
   register.style.transition = "all 1s";
 }
 function shrinkRegister() {
   registerContent.style.visibility = "hidden";
-  register.style.marginTop = shrinkRegisterPx;
+  register.style.bottom = shrinked;
   register.innerText = "Create new account"
   register.style.transition = "all 1s";
 }
 function expandRegister() {
   registerContent.style.visibility = "visible";
-  register.style.marginTop = expandRegisterPx;
+  register.style.bottom = "0";
   register.innerText = "Register"
   register.style.transition = "all 1s";
 }
